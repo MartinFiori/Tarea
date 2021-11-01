2b35f6ccb60e4e94fad78fab3d532dedc9121968
Martín Fiori <martinfiori@outlook.com>
2021-11-01 02:09:12 -0300

    popup

--- js/stock.js
+++ js/stock.js
@@ -6,17 +6,17 @@ let carrito = []
 
 
 
-var checkOpenStatus = function () {
-    var d = new Date();
-    var hour = d.getHours();
-    if (((hour > 12) && (hour < 15)) || (hour >19 && hour <23)) {
-        alert("abierto");
-    } else {
-        alert("cerrado");
-    }
-};
+// var checkOpenStatus = function () {
+//     var d = new Date();
+//     var hour = d.getHours();
+//     if (((hour > 12) && (hour < 15)) || (hour >19 && hour <23)) {
+//         alert("abierto");
+//     } else {
+//         alert("cerrado");
+//     }
+// };
 
-checkOpenStatus();
+// checkOpenStatus();
 
 
 
